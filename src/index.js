1a48621d49f48ec2c2db4604f2514b927d4a95cd
William Fleming <will.fleming12@btinternet.com>
2017-04-21 16:04:48 +0100

    [feat-28] setup single-window electron app to act as a skeleton to build upon

---
+++ src/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+const App = () => {
+  return (
+    <div>
+      <h1>Chattaranga!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+);
