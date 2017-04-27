eca2547f00b1c0e04eb30d91236391001b7a1207
William Fleming <will.fleming12@btinternet.com>
2017-04-27 11:17:36 +0100

    [feat-11] using just the localStream to test the video sizes on the ChatPage

--- src/components/ChatPage/RemoteVideo.js
+++ src/components/ChatPage/RemoteVideo.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const RemoteVideo = () => {
-  return (
-    <div className='remote-video-container'>
-      
-      <video classID='remote-video'>
-        Something Went Wrong :(
-      </video>
+class RemoteVideo extends Component {
+  constructor (props) {
+    super(props);
 
-      <div className='ab-bottom'>
-        <div className='chat-buttons'>
-          <p>BUTTON</p>
+    this.state = {};
+    this.getLocalMedia = this.getLocalMedia.bind(this);
+  }
+
+  componentDidMount () {
+    this.getLocalMedia();
+  }
+
+  getLocalMedia () {
+    navigator.getUserMedia({
+      audio: false,
+      video: true
+    }, stream => {
+      this.setState({
+        localVideo: URL.createObjectURL(stream)
+      });
+    }, () => {});
+  }
+
+  render () {
+    return (
+      <div className='remote-video-container'>
+        
+        <video 
+            src={this.state.localVideo}
+            className='vid' 
+            classID='remote-video' 
+            autoPlay='true'
+            muted='true'>
+          Something Went Wrong :(
+        </video>
+
+        <div className='ab-bottom'>
+          <div className='chat-buttons'>
+            <p>BUTTON</p>
+          </div>
+          <div className='local-video-container'>
+            <video 
+                src={this.state.localVideo}
+                className='vid' 
+                classID='local-video' 
+                autoPlay='true'
+                muted='true'>
+              Something Went Wrong :(
+            </video>
+          </div>
         </div>
-        <div className='local-video-container'>
-          <video classID='local-video'>
-            Something Went Wrong :(
-          </video>
-        </div>
+
       </div>
-
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default RemoteVideo;
