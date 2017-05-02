05662ab6c9649bea5970b4085ae7b17cd93c9aa4
William Fleming <will.fleming12@btinternet.com>
2017-05-02 02:51:34 +0100

    [feat-39] get stat icons to render

--- src/components/HubPage/Icons.js
+++ src/components/HubPage/Icons.js
@@ -4,29 +4,25 @@ import {Link} from 'react-router';
 const Icons = props => {
   return (
     <div className='Icons'>
-     <div className='icondata'>
-        <div className='icon round-icon'>
-            <p>{props.chats}</p>
-        </div>
-     </div>
-    <div className='icondata'>
-        <div className='icon round-icon'>
-            <p>{props.smileys}</p>
-        </div>
-     </div>
-     <div className='icondata'>
-        <div className='icon round-icon'>
-            <p>{props.teacherPoints}</p>
-        </div>
-     </div>
-     <div className='icondata'>
-        <div className='icon round-icon'>
-            <p>{props.talkTime}</p>
-        </div>
-     </div>
-     <div className='icondata achievements'>
-      <Link to='/achievements'><p className='button-primary'>View Badges</p></Link>
-     </div>
+			<div className='icondata'>
+				<img src='img/chatta-icons/chatta-chat.png' className='icon round-icon'/>
+				<p>{props.chats}</p>
+			</div>
+			<div className='icondata'>
+				<img src='img/chatta-icons/chatta-smile.png' className='icon round-icon'/>
+				<p>{props.smileys}</p>
+			</div>
+			<div className='icondata'>
+				<img src='img/chatta-icons/chatta-coach.png' className='icon round-icon'/>
+				<p>{props.teacherPoints}</p>
+			</div>
+			<div className='icondata'>
+				<img src='img/chatta-icons/chatta-time.png' className='icon round-icon'/>
+				<p>{props.talkTime}</p>
+			</div>
+			<div className='icondata achievements'>
+				<Link to='/achievements'><p className='button-primary'>View Badges</p></Link>
+			</div>
     </div>
   );
 };
