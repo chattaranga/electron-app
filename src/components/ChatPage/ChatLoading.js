25cad49d7cc1d924e2105bc1ea3c45035ac8a994
ruthymng <ruth.ym.ng@gmail.com>
2017-05-03 14:37:10 +0100

    Adds loading spinner pre-call

--- src/components/ChatPage/ChatLoading.js
+++ src/components/ChatPage/ChatLoading.js
@@ -4,7 +4,7 @@ import Loading from '../Loading';
 const ChatLoading = () => {
   return (
     <div className='chat-loading'> 
-        <h1>Finding you someone to Chatta with...</h1>
+        <h1>Finding you someone to Chatta with... </h1>
         <Loading/>
     </div>
   );
