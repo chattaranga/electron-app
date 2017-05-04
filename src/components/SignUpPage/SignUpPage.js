f0297c8a246c5deac375f9dca6156617a78cf8ec
acatalina <accarrasco@gmail.com>
2017-05-04 17:01:08 +0100

    [feat-108] add page transitions

--- src/components/SignUpPage/SignUpPage.js
+++ src/components/SignUpPage/SignUpPage.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Animation from 'react-addons-css-transition-group';
 import SignUpForm from './SignUpForm';
 
 const SignUpPage = () => {
   return (
-    <div className='sign-up-page'>
+    <Animation transitionName="page" component="div" className="sign-up-page"
+          transitionAppearTimeout={500} transitionEnterTimeout={500} transitionLeaveTimeout={500}
+          transitionAppear={true} transitionLeave={true}>
       <div className='container'>
         <h1>Hello!</h1>
         <SignUpForm/>
       </div>
-    </div>
+    </Animation>
   );
 };
 
