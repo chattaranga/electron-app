142e890a6a484ffd11319b9c5bc11824726c0a2a
ruthymng <ruth.ym.ng@gmail.com>
2017-05-05 09:55:17 +0100

    Change order of level buttons

--- src/components/SignUpPage/SignUpForm.js
+++ src/components/SignUpPage/SignUpForm.js
@@ -21,7 +21,8 @@ class SignUpForm extends Component {
     this.getError = this.getError.bind(this);
   }
   render () {
-    if (this.props.loading) return <Loading/>;
+
+    if (this.props.loading || this.props.levels.loading || this.props.languages.loading) return <Loading/>;
     if (this.props.user) {
       return (
         <div>
@@ -107,7 +108,7 @@ class SignUpForm extends Component {
     const levelName = this.props.levels.reduce((acc, level) => {
       return level._id === this.props.selectedLevel ? level.name : acc;
     }, '');
-    return levels.map((level, i) => {
+    const mappedLevels = levels.map((level, i) => {
       return (
         <div
             key={i}
@@ -117,6 +118,8 @@ class SignUpForm extends Component {
         </div>
       );
     });
+    const levelsArray = [mappedLevels[2], mappedLevels[0], mappedLevels[1]];
+    return levelsArray;
   }
   addUser (e) {
     e.preventDefault();
